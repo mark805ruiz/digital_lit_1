bb2e717d19e292eb30bab208ecdb7eccfc8eecb5
mark805ruiz <77691714+mark805ruiz@users.noreply.github.com>
2022-04-15 13:05:30 -0600

    Add files via upload

---
+++ 1_4_knowledge_check.py
@@ -0,0 +1,10 @@
+#correct multiple choice answer
+print("\"I am always ready to learn although I do not like being taught.\"");print("by Winston Churchill")
+
+#or we can do this
+
+#print(""""I am always ready to learn although I do not like being taught."
+#by Winston Churchill""")
+
+#or we can also use this
+#print("\"I am always ready to learn although I do not always like being taught.\" \nby Winston Churchill")
